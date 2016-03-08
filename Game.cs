95bce92131e3833b085b8139bc775b20738121c4
djd65 <djd65@zips.uakron.edu>
2016-03-08 14:05:03 -0500

    Initial Commit
    ...

---
+++ Game.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueGame
+{
+    public class Game
+    {
+        public enum GameState
+        {
+            PlayerTurn,
+            BossTurn,
+            EnemyTurn,
+            OtherTurn,
+        }
+
+
+        int OverallState;
+
+        public Game()
+        {
+        }
+
+        public bool IsDone
+        {
+            get;
+            internal set;
+        }
+
+        public void Loop()
+        {
+            Player player = new Player();
+            GameMap map = new GameMap();
+
+            for (;;)
+            {
+                OverallState = (int)GameState.PlayerTurn;
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey();
+                    Console.Write(key);
+                    player.Update(key.KeyChar);
+
+                }
+                OverallState = (int)GameState.BossTurn;
+                //execute boss logic
+                OverallState = (int)GameState.EnemyTurn;
+                //enemy turn logic
+                OverallState = (int)GameState.OtherTurn;
+                //other npc/item logic
+            }
+        }
+    }
+}
